1bf60b941b922742e093b12b1c1ab1af18ac1242
zakkienitz <117953845+zakkienitz@users.noreply.github.com>
2022-11-13 21:27:06 -0700

    Update LogInPage.java

--- LogInPage.java
+++ LogInPage.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-import javafx.event.ActionEvent;
+import javafx.event.ActionEvent; //comment
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
